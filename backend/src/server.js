022215e5a0877f1aaaa67b7b5699c6c2fb4870b4
nasirjd <jafarzadeh.nasir@gmail.com>
2023-10-07 04:49:30 +0330

    20. Publish On Render.com

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 dotenv.config();
-
+import { fileURLToPath } from 'url';
 import express from 'express';
 import cors from 'cors';
 import foodRouter from './routers/food.router.js';
@@ -8,7 +8,11 @@ import userRouter from './routers/user.router.js';
 import orderRouter from './routers/order.router.js';
 
 import { dbconnect } from './config/database.config.js';
+import path, { dirname } from 'path';
 dbconnect();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 app.use(express.json());
@@ -23,7 +27,15 @@ app.use('/api/foods', foodRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
 
-const PORT = 5000;
+const publicFolder = path.join(__dirname, 'public');
+app.use(express.static(publicFolder));
+
+app.get('*', (req, res) => {
+  const indexFilePath = path.join(publicFolder, 'index.html');
+  res.sendFile(indexFilePath);
+});
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log('listening on port ' + PORT);
 });
